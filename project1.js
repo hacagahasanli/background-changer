c53410cefd8df50a691e6960d7f260f63e3a9264
Hesenli-Hacaga <hacaga125+1@gmail.com>
2022-07-21 18:16:03 +0400

    added console.log

--- project1.js
+++ project1.js
@@ -1,4 +1,5 @@
 const hexNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+console.log(hexNumbers);
 const hexBtn = document.querySelector(".hexBtn");
 const bodyBcg = document.querySelector("body");
 const hex = document.querySelector(".hex");
